f0b6b2d7733d61df2dd47b740afc6474f607811c
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-10-27 23:55:20 +0300

    State in compose

--- app/src/main/java/com/alhams/hicomopse/MainActivity.kt
+++ app/src/main/java/com/alhams/hicomopse/MainActivity.kt
@@ -3,8 +3,20 @@ package com.alhams.hicomopse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alhams.hicomopse.ui.theme.HiComopseTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,17 +25,58 @@ class MainActivity : ComponentActivity() {
         setContent {
             HiComopseTheme {
                 // A surface container using the 'background' color from the theme
+                MyApp(Modifier.fillMaxSize())
+            }
+        }
+    }
+}
 
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    var expanded = remember {
+        mutableStateOf<Boolean>(false)
+    }
+
+    var extraPadding = if (expanded.value) 48.dp else 24.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(text = if (expanded.value) "See less" else "See more")
             }
         }
     }
 }
 
 
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     HiComopseTheme {
-
+        MyApp()
     }
 }
