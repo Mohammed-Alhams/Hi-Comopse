c57f52c8f0cc66d9b4db3a2a32dbad38e2685378
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-10-28 00:42:53 +0300

    Creating a performant lazy list

--- app/src/main/java/com/alhams/hicomopse/MainActivity.kt
+++ app/src/main/java/com/alhams/hicomopse/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -38,10 +40,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greetings(
-    modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000){"$it"}
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+//    Column(modifier = modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name = name)
+//        }
+//    }
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names){ name ->
             Greeting(name = name)
         }
     }
